e73311deb5e0e30068ab3ccbe4a05221a35f51b2
Randy-Phillips <attackruby@Randys-MacBook-Pro.local>
2014-07-19 22:38:09 -0400

    Arranged the function calls to match the assignment needs

--- rename-me.js
+++ rename-me.js
@@ -3,8 +3,8 @@
 //
 
 
-var string1 = "my string 1 + ";
-var string2 = "my string 2 is good.";
+var string1 = "my string 1 does not change";
+var string2 = "my string 2 does not change";
 
 
 
@@ -13,19 +13,28 @@ var procedure = confirm("Are you ready?"); if (procedure == true) {
 	}else {console.log("too bad, we are continuing anyways!");
 };
 
-var theString = function () {
+var theString = function (string1, string2) {
 	console.log(string1 + string2) 
 };
 
 
-var myBoolean = function() {var b = confirm("Do you like to count?"); while (b === false) 
+var theBoolean = function() {var b = confirm("Do you like to count?"); while (b === false) 
 	{ var b = confirm("Are you sure...Do you like to count?")
  }
 };
 
 
 
+var theNumber = function (numb) { for (num = 0; num < 10; num++) {
+	console.log("We are currently on, " + num + ". lets continue!")
+  } console.log("We have counted to 10!")
+};
 
 
-theString();
-myBoolean();
+
+theBoolean();
+console.log("lets count 0 to 10 together!");
+theNumber(0);
+theString("Thank you for ","counting with us");
+
+
