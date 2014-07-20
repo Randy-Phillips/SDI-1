f8b64cdcd9545c60ede7eb3c1e8aa1893820d4ff
Randy-Phillips <attackruby@Randys-MacBook-Pro.local>
2014-07-19 21:56:46 -0400

    procedure and string function creation.

--- rename-me.js
+++ rename-me.js
@@ -1,2 +1,20 @@
-alert("JavaScript works!");
-alert("First Commit");
+//
+//
+//
+
+
+var string1 = "my string 1 + ";
+var string2 = "my string 2 is good.";
+
+
+
+var procedure = confirm("Are you ready?"); if (procedure == true) {
+	console.log(" you are ready, lets continue.");
+	}else {console.log("too bad, we are continuing anyways!");
+};
+
+var theString = function () {
+	console.log(string1 + string2) 
+};
+
+theString();
