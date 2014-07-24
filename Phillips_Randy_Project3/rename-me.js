e2f1a6532e7166f65511da29c75b5e1c0726d7bb
Randy-Phillips <attackruby@Randys-MacBook-Pro.local>
2014-07-24 16:59:06 -0400

    Working Story. Working on getStore Variable.

--- Phillips_Randy_Project3/rename-me.js
+++ Phillips_Randy_Project3/rename-me.js
@@ -1,1 +1,46 @@
-alert("JavaScript works!");
+// myName =  Randy Phillips
+// myClass = SDI 1407
+// myProject = Project 3
+
+
+// Procedure
+
+var startMessage = function start() {
+	console.log("It's time for work!");
+}
+
+
+// Array - Store Names
+
+var storeNames = ["Circle K","Walmart", "Sheetz"];
+
+//JSON Data
+
+var myInfo = [
+    {"firstName":"Randy", "lastName":"Phillips"},
+    {"totalMoney":"Total Cash", "cashValue":20.00},
+   
+];
+
+// Number Argument + Function
+
+var getCash = function getCash(cash) {
+	(myInfo[1].cashValue = cash);
+	console.log("I walk out the door, with $" + (myInfo[1].cashValue) + " in my pocket");
+		 
+};
+
+
+// Store Variable 
+
+var getStore;
+
+console.log(myInfo[0].firstName + " " + myInfo[0].lastName + " get's ready for work...");
+startMessage();
+getCash(15.00);
+
+
+
+
+
+
